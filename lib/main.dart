0a274ac50630ebc05f160053264f8b24b10cf8f0
alan <alanekgladys117@gmail.com>
2024-11-21 12:43:27 +0100

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:purrfectshop_app/auth/login_screen.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
@@ -12,7 +12,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,8 +21,47 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MaterialApp(
-          debugShowCheckedModeBanner: false, home: LoginScreen()),
+      home: const SplashScreen(), // Ustaw SplashScreen jako ekran początkowy
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToLogin();
+  }
+
+  void _navigateToLogin() {
+    // Przejście do ekranu logowania po 2 sekundach
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF5F0F40),
+      body: Center(
+        child: Image.asset(
+          'images/logoCatGold.png', // Ścieżka do obrazka
+          fit: BoxFit.cover, // Dopasowanie obrazu
+          height: 300,
+          width: 300,
+        ),
+      ),
     );
   }
 }
