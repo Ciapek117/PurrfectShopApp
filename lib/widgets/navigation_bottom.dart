263c234c8bbe1ebd75b920e7a0db9234b33c29ca
MartiHed <martyna.hedeszynska@gmail.com>
2024-11-21 13:13:53 +0100

    made better nav look

--- lib/widgets/navigation_bottom.dart
+++ lib/widgets/navigation_bottom.dart
@@ -10,7 +10,7 @@ class MyNavBarBottom extends StatefulWidget {
 }
 
 class _MyNavBarBottomState extends State<MyNavBarBottom> {
-  int _selectedIndex = 1;
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -18,15 +18,15 @@ class _MyNavBarBottomState extends State<MyNavBarBottom> {
         padding: const EdgeInsets.symmetric(vertical: 10),
         child: GNav(
             tabBorder: Border(
-                top: BorderSide(color: Color(0xFFD2AF43), width: 2, strokeAlign: 3),
-                bottom: BorderSide(color: Color(0xFFD2AF43), width: 2, strokeAlign: 3),
-                left: BorderSide(color: Color(0xFFD2AF43), width: 2, strokeAlign: 3),
-                right: BorderSide(color: Color(0xFFD2AF43), width: 2, strokeAlign: 3)
+                top: BorderSide(color: Color(0xFFD2AF43), width: 3, strokeAlign: 3),
+                bottom: BorderSide(color: Color(0xFFD2AF43), width: 3, strokeAlign: 3),
+                left: BorderSide(color: Color(0xFFD2AF43), width: 3, strokeAlign: 3),
+                right: BorderSide(color: Color(0xFFD2AF43), width: 3, strokeAlign: 3)
             ),
           //backgroundColor: Color(0xFFD2AF43),
             color: const Color(0xFFD2AF43),
             activeColor: const Color(0xFF5F0F40),
-            tabActiveBorder: Border.all(color: const Color(0xFFD2AF43), strokeAlign: 3, width: 2),
+            tabActiveBorder: Border.all(color: const Color(0xFFD2AF43), strokeAlign: 3, width: 3),
             tabBackgroundColor: const Color(0xFFD2AF43),
             mainAxisAlignment: MainAxisAlignment.spaceAround,
 
@@ -39,24 +39,26 @@ class _MyNavBarBottomState extends State<MyNavBarBottom> {
                return widget.onTabChange!(value);
             },
             tabs: [
-              const GButton(icon: Icons.shopping_bag_rounded, text: 'Cart',iconColor: Color(0xFFD2AF43),),
               GButton(
                 icon: Icons.shopping_bag_rounded, // Placeholder icon (required)
                 leading: Container(
-                  child: Image.asset(
-                      'images/catWithoutName.png', // Replace with your image path
-                      width: 25,
-                      height: 25,
-                      fit: BoxFit.cover,
-                      color: (_selectedIndex == 1) ? const Color(0xFF5F0F40) : const Color(0xFFD2AF43)
+                    child: Image.asset(
+                        'images/catWithoutName.png', // Replace with your image path
+                        width: 25,
+                        height: 25,
+                        fit: BoxFit.cover,
+                        color: (_selectedIndex == 0) ? const Color(0xFF5F0F40) : const Color(0xFFD2AF43)
+                    ),
                   ),
-                ),
-                text: 'Home',
+                text: 'Home', textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color:Color(0xFF5F0F40,)),
               ),
 
-              const GButton(icon: Icons.photo, text: 'Album', iconColor: Color(0xFFD2AF43)),
 
-              const GButton(icon: Icons.logout, text: 'Logout', iconColor: Color(0xFFD2AF43))
+              const GButton(icon: Icons.photo, text: 'Album', iconColor: Color(0xFFD2AF43), textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color:Color(0xFF5F0F40) )),
+
+              const GButton(icon: Icons.shopping_bag_rounded, text: 'Cart',iconColor: Color(0xFFD2AF43), textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color:Color(0xFF5F0F40) )),
+
+              const GButton(icon: Icons.logout, text: 'Logout', iconColor: Color(0xFFD2AF43), textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color:Color(0xFF5F0F40) ))
             ])
     );
   }
