21e4a201cf63c3ba1e2bef272a928044ed1ab66d
alan <alanekgladys117@gmail.com>
2024-11-21 09:35:13 +0100

    initial commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -16,7 +16,7 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text(
-              "Welcome User",
+              "Welcome users",
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
             ),
             const SizedBox(height: 20),
