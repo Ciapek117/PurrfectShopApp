b5f21e39d274b92af20972f42cac435856bcb5ac
MartiHed <martyna.hedeszynska@gmail.com>
2024-11-21 09:59:02 +0100

    changed button in button.dart

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'dart:ui';
 
-class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.label, this.onPressed});
+class CustomButton extends StatefulWidget {
+  const CustomButton({super.key, required this.label, required this.onPressed});
   final String label;
   final void Function()? onPressed;
 
+  @override
+  State<CustomButton> createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -13,13 +18,15 @@ class CustomButton extends StatelessWidget {
         height: 42,
         margin: EdgeInsets.symmetric(horizontal: 90),
         child: ElevatedButton(
-            onPressed: onPressed,
+            onPressed: widget.onPressed,
+            onHover: (val) => {
+            },
             style: ElevatedButton.styleFrom(
               backgroundColor: Color(0xFFD2AF43),
 
             ),
             child: Text(
-              label,
+              widget.label,
               style: const TextStyle(fontSize: 18, color: Color(0xFF5F0F40), fontWeight: FontWeight.bold),
             )));
   }
