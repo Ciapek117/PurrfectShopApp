78d61746cd055f718baf796b045be06d7c692f6a
MartiHed <martyna.hedeszynska@gmail.com>
2024-11-21 13:53:34 +0100

    Pages upgrade

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -5,6 +5,9 @@ class CartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0xFF5F0F40),
+      body: Text('Cart Page'),
+    );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,6 +5,9 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0xFF5F0F40),
+      body: Text('HomePage'),
+    );
   }
 }
