eb0c0a8b70188075d1ae72dc1c9444ac30d3889b
MartiHed <martyna.hedeszynska@gmail.com>
2024-11-22 12:46:54 +0100

    home_page is now scrollable in vertical axis

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,40 +9,42 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFF5F0F40),
-      body: Column(
-        children:[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-            child: SizedBox(
-                height: 130,
-                child: ListView.builder(
-                  itemCount: 5,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) => CustomTile(
-                      backgroundColor: Color(0xFF61ccee).withOpacity(0.5),
-                      text: '-50% OFF!',
-                      imagePath: 'images/images_for_tiles/kot1.jpg',
-                      width: 220,
-                  ))),
-          ),
-
-          const SizedBox(height: 5),
-
-          Padding(
-            padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
-            child: SizedBox(
-                height: 450,
-                child: ListView.builder(
-                    itemCount: 7,
+      body: SingleChildScrollView(
+        child: Column(
+          children:[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+              child: SizedBox(
+                  height: 130,
+                  child: ListView.builder(
+                    itemCount: 5,
                     scrollDirection: Axis.horizontal,
                     itemBuilder: (context, index) => CustomTile(
-                      backgroundColor: Color(0xFF61ccee).withOpacity(0.5),
-                      text: '-50% OFF!',
-                      imagePath: 'images/images_for_tiles/kot1.jpg',
-                      width: 350,
+                        backgroundColor: Color(0xFF61ccee).withOpacity(0.5),
+                        text: '-50% OFF!',
+                        imagePath: 'images/images_for_tiles/kot1.jpg',
+                        width: 220,
                     ))),
-          ),
-        ],
+            ),
+        
+            const SizedBox(height: 5),
+        
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
+              child: SizedBox(
+                  height: 450,
+                  child: ListView.builder(
+                      itemCount: 7,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) => CustomTile(
+                        backgroundColor: Color(0xFF61ccee).withOpacity(0.5),
+                        text: '-50% OFF!',
+                        imagePath: 'images/images_for_tiles/kot1.jpg',
+                        width: 350,
+                      ))),
+            ),
+          ],
+        ),
       ),
     );
   }
