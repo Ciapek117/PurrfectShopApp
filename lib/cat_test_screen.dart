f77b629f0b662247572a1d328dd6f93290575c6f
alan <alanekgladys117@gmail.com>
2024-11-25 13:22:31 +0100

    Updated pulling info from firebase

--- lib/cat_test_screen.dart
+++ lib/cat_test_screen.dart
@@ -7,7 +7,7 @@ class CatImageScreen extends StatefulWidget {
 }
 
 class _CatImageScreenState extends State<CatImageScreen> {
-  List<String> images = [];
+  Map<String, Map<String, String>> images = {};
   bool isLoading = true;
 
   @override
@@ -18,20 +18,29 @@ class _CatImageScreenState extends State<CatImageScreen> {
 
   Future<void> getCatImage() async {
     try {
-      List<String> tempImages = [];
+      Map<String, Map<String, String>> tempImages = {};
 
+      // Pobieranie obrazków i wypełnianie słownika
       for (var i = 1; i <= 4; i++) {
+        final docId = 'cat$i'; // Identyfikator dokumentu
         final doc = await FirebaseFirestore.instance
             .collection('catphotos')
-            .doc('cat$i')
+            .doc(docId)
             .get();
 
+        // Tymczasowa lista do ktorej wprowadzane sa dane
         if (doc.exists) {
-          tempImages.add(doc['imageUrl']);
+          tempImages[docId] = {
+            'description': doc['description'],
+            'imageUrl': doc['imageUrl'],
+            'name': doc['name'],
+            'price': doc['price'],
+            'tag': doc['tag'],
+          };
         }
       }
 
-      // Ustawienie danych i zakończenie ładowania
+      // Ustawienie danych do glownego slownika i zakończenie ładowania
       setState(() {
         images = tempImages;
         isLoading = false;
@@ -54,20 +63,53 @@ class _CatImageScreenState extends State<CatImageScreen> {
     }
 
     return Column(
-      children: images.map((img) {
+      children: images.entries.map((entry) {
+        final docId = entry.key; // Identyfikator kotka
+        final imgData = entry.value; // Dane obrazka (imageUrl, name itp.)
+
+        final description = imgData['description'];
+        final img = imgData['imageUrl'];
+        final name = imgData['name'];
+        final price = imgData['price'];
+        final tag = imgData['tag'];
+
         return Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Image.network(
-            img,
-            width: 100,
-            height: 100,
-            loadingBuilder: (context, child, loadingProgress) {
-              if (loadingProgress == null) return child;
-              return Center(child: CircularProgressIndicator());
-            },
-            errorBuilder: (context, error, stackTrace) {
-              return Text('Nie udało się załadować obrazu.');
-            },
+          child: Column(
+            children: [
+              Text(
+                'ID: $docId',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Name: $name',
+                style: TextStyle(fontSize: 16),
+              ),
+              Text(
+                'Description: $description',
+                style: TextStyle(fontSize: 16),
+              ),
+              Text(
+                'price: $price',
+                style: TextStyle(fontSize: 16),
+              ),
+              Text(
+                'tag: $tag',
+                style: TextStyle(fontSize: 16),
+              ),
+              Image.network(
+                img!,
+                width: 100,
+                height: 100,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) return child;
+                  return Center(child: CircularProgressIndicator());
+                },
+                errorBuilder: (context, error, stackTrace) {
+                  return Text('Nie udało się załadować obrazu.');
+                },
+              ),
+            ],
           ),
         );
       }).toList(),
@@ -80,7 +122,9 @@ class _CatImageScreenState extends State<CatImageScreen> {
       appBar: AppBar(
         title: Text('Cat Photo'),
       ),
-      body: createCatTile(),
+      body: SingleChildScrollView(
+        child: createCatTile(),
+      ),
     );
   }
 }
