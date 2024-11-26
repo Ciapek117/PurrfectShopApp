a03a7ec59ee105e5288bb5f86237cf0f32f03b1f
MartiHed <martyna.hedeszynska@gmail.com>
2024-11-26 13:54:59 +0100

    nie pamietam co zrobilam :<

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:glowy_borders/glowy_borders.dart';
+import '../models/product.dart';
 import '../widgets/custom_tile.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    _loadProducts();
+  }
+
+  Future<void> _loadProducts() async {
+    try {
+      final QuerySnapshot snapshot = await FirebaseFirestore.instance
+          .collection('catgifs') // Twoja kolekcja produktów
+          .get();
+
+      if (snapshot.docs.isNotEmpty) {
+        List<String> paths = snapshot.docs.map((doc) => doc['gifPath'] as String).toList();
+        print(paths);
+      } else {
+        print("Brak produktów w kolekcji.");
+      }
+    } catch (e) {
+      print("Błąd podczas ładowania produktów: $e");
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -26,9 +58,13 @@ class HomePage extends StatelessWidget {
                         width: 220,
                     ))),
             ),
-        
+
             const SizedBox(height: 5),
-        
+
+
+            //TODO: zrobic jednego tile'a, ktory bedzie mial przykladowo 7 gifow z firebase'a
+            //TODO: za pomoca duration zmieniac te gify w kolko (np. co 3sek)
+
             Padding(
               padding: const EdgeInsets.fromLTRB(10, 10, 10, 20),
               child: SizedBox(
--- lib/widgets/custom_tile.dart
+++ lib/widgets/custom_tile.dart
@@ -10,43 +10,81 @@ class CustomTile extends StatelessWidget {
   final String imagePath;
   final double width;
 
-  CustomTile({super.key, required this.backgroundColor, required this.text, required this.imagePath, required this.width});
+  CustomTile({
+    super.key,
+    required this.backgroundColor,
+    required this.text,
+    required this.imagePath,
+    required this.width,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedGradientBorder(
-      gradientColors: [
-        Color(0xFFD2AF43),
-        Colors.transparent,
-        Colors.transparent,
-        Color(0xFFD2AF43),
-      ],
-
-      glowSize: 1,
-      borderRadius: BorderRadius.circular(30),
-
-      child: Padding(
-        padding: const EdgeInsets.all(0.001),
+    return Padding(
+      padding: const EdgeInsets.all(5.0), // Lepsza przestrzeń wokół
         child: Container(
           width: width,
           decoration: BoxDecoration(
+            border: Border.all(
+              color: Color(0xFFD2AF43),
+              width: 3,
+              strokeAlign: 4
+            ),
             borderRadius: BorderRadius.circular(20),
             image: DecorationImage(
               image: AssetImage(imagePath),
-              fit: BoxFit.cover)),
-
+              fit: BoxFit.cover,
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                blurRadius: 10,
+                offset: Offset(0, 4),
+              ),
+            ],
+          ),
           padding: EdgeInsets.all(20),
-          margin: EdgeInsets.all(7),
-          child: Center(child: Text(text,style: GoogleFonts.bebasNeue(textStyle: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.w800,
-            backgroundColor: backgroundColor,
-            letterSpacing: 2,
-
-          )))),
+          margin: EdgeInsets.all(5),
+          child: Stack(
+            children: [
+              // Gradient background
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(color: backgroundColor.withOpacity(0.2))
+                  ],
+                  gradient: LinearGradient(
+                    colors: [Colors.transparent, backgroundColor.withOpacity(0.6)],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+              ),
+              // Centered Text
+              Center(
+                child: Text(
+                  text,
+                  style: GoogleFonts.bebasNeue(
+                    textStyle: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w800,
+                      color: Colors.white,
+                      letterSpacing: 2,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: Colors.black.withOpacity(0.5),
+                          offset: Offset(2, 2),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
     );
-
   }
 }
