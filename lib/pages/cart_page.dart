74d557ca58c33defc0493f464daa19460290c0fc
alan <alanekgladys117@gmail.com>
2024-11-26 13:06:05 +0100

    cart :D

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/cart.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF5F0F40),
-      body: Text('Cart Page'),
+    return Consumer<Cart>(builder: (context, value, child) => Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Column(
+        children: [
+          Text('Moj koszyk', style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,),
+          ),
+          
+          const SizedBox(height: 10,)
+
+        ],
+      ),
+    ),
     );
   }
 }
